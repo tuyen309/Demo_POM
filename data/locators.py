9c8fb1525400ebe9a4150e0b5f7f70d319279aeb
tuyen309 <56708673+tuyen309@users.noreply.github.com>
2024-12-23 14:51:52 +0700

    Create locators.py

---
+++ data/locators.py
@@ -0,0 +1,15 @@
+class LoginPageLocators:
+    USERNAME_INPUT = "//*[@id='user-name']"
+    PASSWORD_INPUT = "//*[@id='password']"
+    LOGIN_BUTTON = "//*[@id='login-button']"
+
+class RegisterPageLocators:
+    FIRSTNAME_INPUT = "//*[@id='firstname']"
+    LASTNAME_INPUT = "//*[@id='lastname']"
+    EMAIL_INPUT = "//*[@id='email']"
+    PASSWORD_INPUT = "//*[@id='password']"
+    REGISTER_BUTTON = "//*[@id='register']"
+
+class LogoutPageLocators:
+    LOGOUT_BUTTON = "//*[text()='Logout']"
+    MENU_BUTTON = "//*[text()='Open Menu']"
